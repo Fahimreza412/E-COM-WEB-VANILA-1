e2c0c0412a4d740c55a655371aeae4c9cd2e139b
Fahimreza412 <fahimreza2210622123@gmail.com>
2025-02-13 03:58:15 +0600

    1.2

---
+++ vite-project/src/homeProductCards.js
@@ -0,0 +1,7 @@
+const showProductContainer = document.querySelector('#productContainer')
+const showProductTemplate = document.querySelector('#productTemplate')
+
+export const showProdcutContainer = (products) => {
+
+    
+}
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,7 @@
 import './style.css'
+import products from './API/Products.json';
+console.log(products);
+import { showProdcutContainer } from './homeProductCards';
+// !Define a function named showProductContainer that takes an array of products as input.
+
+showProductContainer(products)
